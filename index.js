13a3a77481720e79505dc44b8748deb4b9372826
TalitaFernandes <talitafernandesvilas@gmail.com>
2025-09-22 16:10:51 -0300

    meu primeiro

---
+++ index.js
@@ -0,0 +1,17 @@
+let amanhaMercado = ("comprar abacaxi")
+let amanhaEscola = (" comer bolo")
+let amanhaIgreja = ("ler a biblia")
+
+
+console.log 
+("As segundas-feiras eu  " + amanhaIgreja + 
+    " e depois vou para a escola e "+ amanhaEscola + " não posso me esquecer de " + amanhaMercado)
+
+amanhaMercado = ("comprar abobora")
+//alterei a variavel amanhaMercado de abacaxi virou abobora
+
+console.log 
+("As segundas-feiras eu  " + amanhaIgreja + 
+    " e depois vou para a escola e "+ amanhaEscola + " não posso me esquecer de " + amanhaMercado)
+console.log
+ ("melhor, minha mãe pediu para comprar " + amanhaMercado + " não posso esquecer.")
